c869e317d1cffc5be56b588ecaf470aa03192932
Jack Bransfield <bransfieldjack@gmail.com>
2018-08-19 01:57:12 +0000

    Added task submission functionality.

--- app.py
+++ app.py
@@ -1,28 +1,35 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = "task_manager"
-app.config["MONGO_URI"] = "mongodb://admin:password1@ds121088.mlab.com:21088/task_manager"
-
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb://admin:admin@ds123926.mlab.com:23926/task_manager'
 
 mongo = PyMongo(app)
 
-
 @app.route('/')
-@app.route("/get_tasks")
+@app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
-
-
-@app.route("/add_task")
+    return render_template("tasks.html", 
+    tasks=mongo.db.tasks.find())
+    
+    
+@app.route('/add_task')
 def add_task():
-    return render_template("addtask.html", categories=mongo.db.categories.find())
-
+    return render_template('addtask.html',
+    categories=mongo.db.categories.find())
+    
+    
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks =  mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+    
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+        port=int(os.environ.get('PORT')),
+debug=True)
